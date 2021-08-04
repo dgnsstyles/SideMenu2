3f2f2a1bd35080101b0c4f2c6b7dd3ea859a14a4
David Goren <david.goren_ex@scotiabank.cl>
2021-08-04 17:51:31 -0400

    Initial Commit

---
+++ SideMenu2/SideMenu2App.swift
@@ -0,0 +1,17 @@
+//
+//  SideMenu2App.swift
+//  SideMenu2
+//
+//  Created by usuario on 04-08-21.
+//
+
+import SwiftUI
+
+@main
+struct SideMenu2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
